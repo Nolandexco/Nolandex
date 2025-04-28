62315e88b4f3fb61c99bff6320f455bf1077b9e1
Nolandexco <stemapal321@gmail.com>
2025-04-29 06:50:35 +0700

    Update logos.ts

--- config/logos.ts
+++ config/logos.ts
@@ -1,35 +1,34 @@
-
 export const LOGOS = [
   {
     name: "Next.js",
-    image: "/images/techStack/nextjs.svg",
+    image: "/images/techStack/logo1.jpg",
   },
   {
     name: "React",
-    image: "/images/techStack/react.svg",
+    image: "/images/techStack/logo2.jpg",
   },
   {
     name: "Tailwind",
-    image: "/images/techStack/tailwind.svg",
+    image: "/images/techStack/logo3.jpg",
   },
   {
     name: "Framer",
-    image: "/images/techStack/framer.svg",
+    image: "/images/techStack/logo4.jpg",
   },
   {
     name: "Shadcnui",
-    image: "/images/techStack/shadcnui.svg",
+    image: "/images/techStack/logo5.jpg",
   },
   {
     name: "Nextui",
-    image: "/images/techStack/nextui.svg",
+    image: "/images/techStack/logo6.jpg",
   },
   {
     name: "TS",
-    image: "/images/techStack/typescript.svg",
+    image: "/images/techStack/logo7.jpg",
   },
   {
     name: "Vercel",
-    image: "/images/techStack/vercel.svg",
+    image: "/images/techStack/logo8.jpg",
   },
 ];
