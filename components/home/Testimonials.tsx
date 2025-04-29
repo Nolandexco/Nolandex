d87d295ab004526d3a6a65684ee9204499528ccf
Nolandexco <stemapal321@gmail.com>
2025-04-29 13:12:24 +0700

    Update Testimonials.tsx

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -5,6 +5,8 @@ import { TestimonialsData } from "@/config/testimonials";
 import Image from "next/image";
 import Link from "next/link";
 import { RoughNotation } from "react-rough-notation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
   return (
@@ -19,7 +21,6 @@ const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
           </RoughNotation>
         </h2>
         <p className="text-large text-default-500">
-          {/* Don't take our word for it. Here's what they have to say. */}
           {locale.description1}{" "}
           <Link
             href={siteConfig.authors[0].twitter as string}
@@ -32,10 +33,20 @@ const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
           {locale.description3}
         </p>
       </div>
-      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-4 overflow-hidden relative transition-all">
+
+      <Swiper
+        spaceBetween={16}
+        slidesPerView={1}
+        breakpoints={{
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
+        className="w-full"
+      >
         {TestimonialsData.map((testimonial, index) => (
-          <div className="mb-4 z-0 break-inside-avoid-column" key={index}>
-            <div className="border border-slate/10 rounded-lg p-4 flex flex-col items-start gap-3 h-fit">
+          <SwiperSlide key={index}>
+            <div className="border border-slate/10 rounded-lg p-4 flex flex-col items-start gap-3 h-full bg-background">
               <div className="flex items-start justify-between w-full">
                 <div className="flex items-start gap-2">
                   <Image
@@ -64,9 +75,9 @@ const Testimonials = ({ id, locale }: { id: string; locale: any }) => {
                 {testimonial.content}
               </p>
             </div>
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </section>
   );
 };
