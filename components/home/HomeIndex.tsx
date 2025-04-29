3a0c3a7b65e6831a7499f18c3596a847b9762484
Nolandexco <stemapal321@gmail.com>
2025-04-30 00:42:10 +0700

    Add files via upload

--- components/home/HomeIndex.tsx
+++ components/home/HomeIndex.tsx
@@ -1,34 +1,41 @@
-// components/home/HomeIndex.tsx
-import dynamic from "next/dynamic";
-import { defaultLocale, getDictionary } from "@/lib/i18n";
-import Feature from "@/components/home/Feature";
+import CTA from "@/components/home/CTA";
 import FAQ from "@/components/home/FAQ";
+import Feature from "@/components/home/Feature";
+import Hero from "@/components/home/Hero";
+import Pricing from "@/components/home/Pricing";
+import ScrollingLogos from "@/components/home/ScrollingLogos";
+import SocialProof from "@/components/home/SocialProof";
+import Testimonials from "@/components/home/Testimonials";
+import { defaultLocale, getDictionary } from "@/lib/i18n";
 
-// Client components
-const Hero = dynamic(() => import("@/components/home/Hero"), { ssr: false });
-const Pricing = dynamic(() => import("@/components/home/Pricing"), { ssr: false });
-const CTA = dynamic(() => import("@/components/home/CTA"), { ssr: false });
-const ScrollingLogos = dynamic(() => import("@/components/home/ScrollingLogos"), { ssr: false });
-const SocialProof = dynamic(() => import("@/components/home/SocialProof"), { ssr: false });
-const Testimonials = dynamic(() => import("@/components/home/Testimonials"), { ssr: false });
-
-interface HomeIndexProps {
-  params: { lang: string };
-}
-
-export default async function HomeIndex({ params }: HomeIndexProps) {
-  const langName = params?.lang || defaultLocale;
+export default async function HomeIndex({ lang }: { lang: string }) {
+  const langName = lang || defaultLocale;
   const dict = await getDictionary(langName);
 
   return (
     <>
+      {/* Hero Section */}
       <Hero locale={dict.Hero} langName={langName} CTALocale={dict.CTAButton} />
       <SocialProof locale={dict.SocialProof} />
+      {/* display technology stack, partners, project honors, etc. */}
       <ScrollingLogos />
+
+      {/* Showcase */}
+      {/* <Showcase id="Showcase" locale={dict.Showcase} /> */}
+
+      {/* USP (Unique Selling Proposition) */}
       <Feature id="Features" locale={dict.Feature} langName={langName} />
-      <Pricing locale={dict.Pricing} langName={langName} />
+
+      {/* Pricing */}
+      <Pricing id="Pricing" locale={dict.Pricing} langName={langName} />
+
+      {/* Testimonials */}
       <Testimonials id="Testimonials" locale={dict.Testimonials} />
+
+      {/* FAQ (Frequently Asked Questions) */}
       <FAQ id="FAQ" locale={dict.FAQ} langName={langName} />
+
+      {/* CTA (Call to Action) */}
       <CTA locale={dict.CTA} CTALocale={dict.CTAButton} />
     </>
   );
