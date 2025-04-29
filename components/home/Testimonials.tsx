41e83181b96cb54fed895dadccb0dee89fb21ba5
Nolandexco <stemapal321@gmail.com>
2025-04-29 13:20:58 +0700

    Update Testimonials.tsx

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -1,4 +1,5 @@
-/* eslint-disable react/no-unescaped-entities */
+"use client";
+
 import { TwitterX } from "@/components/social-icons/icons";
 import { siteConfig } from "@/config/site";
 import { TestimonialsData } from "@/config/testimonials";
